503ffd09a1c3e979c1d9798d8211a19178d432ae
kadir <akadiratar@gmail.com>
2021-01-05 00:01:32 +0300

    App.js created

---
+++ src/App/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { StatusBar, StyleSheet } from 'react-native';
+import { View } from 'native-base';
+
+import Splash from '../Splash';
+import Home from '../Home';
+
+import Background from '../Components/Background';
+import { splashback } from '../Assets';
+import { w } from '../Dimensions';
+
+
+const RENDER = () => {
+    const [splash, setSplash] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => setSplash(false), 2000);
+
+        return () => null;
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle={'dark-content'} translucent backgroundColor={'transparent'} />
+            <Background source={splashback} opacity={0.4} />
+            {splash ? <Splash /> : <Home />}
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        width: w,
+        height: '100%'
+    }
+});
+
+
+export default RENDER;
